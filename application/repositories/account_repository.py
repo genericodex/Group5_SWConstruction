5166dded9abb6c176da1f11684db502cb3a2a584
helviz <twomoelvisrodney@gmail.com>
2025-04-11 00:19:19 +0300

    application layer tests

--- application/repositories/account_repository.py
+++ application/repositories/account_repository.py
@@ -1,54 +1,53 @@
 from abc import ABC, abstractmethod
-from typing import Dict, List
+from typing import List, Optional
 from domain.accounts import Account
 
 
-# Abstract Base Class for AccountRepository
-class AccountRepository(ABC):
+class IAccountRepository(ABC):
     @abstractmethod
     def save(self, account: Account) -> None:
-        """Save or update an account in the repository."""
-        ...
+        """Save or update an account in the repository"""
+        pass
 
     @abstractmethod
-    def get_by_id(self, account_id: str) -> Account:
-        """Retrieve an account by its unique ID."""
-        ...
+    def find_by_id(self, account_id: str) -> Optional[Account]:
+        """Find an account by its ID"""
+        pass
 
     @abstractmethod
-    def get_all(self) -> List[Account]:
-        """Retrieve all accounts."""
-        ...
+    def find_all(self) -> List[Account]:
+        """Find all accounts"""
+        pass
 
     @abstractmethod
     def delete(self, account_id: str) -> None:
-        """Delete an account by its unique ID."""
-        ...
+        """Delete an account from the repository"""
+        pass
 
 
-# Concrete implementation using in-memory dictionary
-class InMemoryAccountRepository(AccountRepository):
-    def __init__(self):
-        self._accounts: Dict[str, Account] = {}
-
-    def save(self, account: Account) -> None:
-        """Insert or update an account."""
-        self._accounts[account.account_id] = account
-
-    def get_by_id(self, account_id: str) -> Account:
-        """Fetch an account by ID."""
-        account = self._accounts.get(account_id)
-        if not account:
-            raise ValueError(f"Account with ID '{account_id}' not found.")
-        return account
-
-    def get_all(self) -> List[Account]:
-        """Return a list of all accounts."""
-        return list(self._accounts.values())
-
-    def delete(self, account_id: str) -> None:
-        """Remove an account by ID."""
-        if account_id in self._accounts:
-            del self._accounts[account_id]
-        else:
-            raise ValueError(f"Account with ID '{account_id}' does not exist.")
+# # Concrete implementation using in-memory dictionary
+# class InMemoryAccountRepository(AccountRepository):
+#     def __init__(self):
+#         self._accounts: Dict[str, Account] = {}
+#
+#     def save(self, account: Account) -> None:
+#         """Insert or update an account."""
+#         self._accounts[account.account_id] = account
+#
+#     def get_by_id(self, account_id: str) -> Account:
+#         """Fetch an account by ID."""
+#         account = self._accounts.get(account_id)
+#         if not account:
+#             raise ValueError(f"Account with ID '{account_id}' not found.")
+#         return account
+#
+#     def get_all(self) -> List[Account]:
+#         """Return a list of all accounts."""
+#         return list(self._accounts.values())
+#
+#     def delete(self, account_id: str) -> None:
+#         """Remove an account by ID."""
+#         if account_id in self._accounts:
+#             del self._accounts[account_id]
+#         else:
+#             raise ValueError(f"Account with ID '{account_id}' does not exist.")
--- application/repositories/transaction_repository.py
+++ application/repositories/transaction_repository.py
@@ -1,48 +1,48 @@
 from abc import ABC, abstractmethod
-from typing import List, Dict
+from typing import List, Optional
 from domain.transactions import Transaction
 
 
-class TransactionRepository(ABC):
+class ITransactionRepository(ABC):
     @abstractmethod
     def save(self, transaction: Transaction) -> None:
-        """Save a transaction."""
-        ...
+        """Save a transaction to the repository"""
+        pass
 
     @abstractmethod
-    def get_by_id(self, transaction_id: str) -> Transaction:
-        """Retrieve a transaction by its ID."""
-        ...
+    def find_by_id(self, transaction_id: str) -> Optional[Transaction]:
+        """Find a transaction by its ID"""
+        pass
 
     @abstractmethod
-    def get_by_account_id(self, account_id: str) -> List[Transaction]:
-        """Retrieve all transactions for a specific account."""
-        ...
+    def find_by_account_id(self, account_id: str) -> List[Transaction]:
+        """Find all transactions for a specific account"""
+        pass
 
     @abstractmethod
-    def get_all(self) -> List[Transaction]:
-        """Retrieve all transactions."""
-        ...
+    def find_all(self) -> List[Transaction]:
+        """Find all transactions"""
+        pass
 
 
-class InMemoryTransactionRepository(TransactionRepository):
-    def __init__(self):
-        self._transactions: Dict[str, Transaction] = {}
-
-    def save(self, transaction: Transaction) -> None:
-        self._transactions[transaction.transaction_id] = transaction
-
-    def get_by_id(self, transaction_id: str) -> Transaction:
-        transaction = self._transactions.get(transaction_id)
-        if not transaction:
-            raise ValueError(f"Transaction with ID '{transaction_id}' not found.")
-        return transaction
-
-    def get_by_account_id(self, account_id: str) -> List[Transaction]:
-        return [
-            txn for txn in self._transactions.values()
-            if txn.account_id == account_id
-        ]
-
-    def get_all(self) -> List[Transaction]:
-        return list(self._transactions.values())
+# class InMemoryTransactionRepository(TransactionRepository):
+#     def __init__(self):
+#         self._transactions: Dict[str, Transaction] = {}
+#
+#     def save(self, transaction: Transaction) -> None:
+#         self._transactions[transaction.transaction_id] = transaction
+#
+#     def get_by_id(self, transaction_id: str) -> Transaction:
+#         transaction = self._transactions.get(transaction_id)
+#         if not transaction:
+#             raise ValueError(f"Transaction with ID '{transaction_id}' not found.")
+#         return transaction
+#
+#     def get_by_account_id(self, account_id: str) -> List[Transaction]:
+#         return [
+#             txn for txn in self._transactions.values()
+#             if txn.account_id == account_id
+#         ]
+#
+#     def get_all(self) -> List[Transaction]:
+#         return list(self._transactions.values())
